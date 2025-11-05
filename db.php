ecb07791767a745d0a5c15c3334caf8343d5259f
Julia Izabel <julia_roza@estudante.sesisenai.org.br>
2025-11-05 08:15:37 -0300

    add pagina db

---
+++ db.php
@@ -0,0 +1,27 @@
+<?php
+
+$servername = "localhost";
+$username = "root";
+$password = "root";
+$dbname = "fast_sesi_sa";
+try {
+    $pdo = new PDO("mysql:host=$DB_HOST;dbname=$DB_NAME;charset=utf8mb4", $DB_USER, $DB_PASS, [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+    ]);
+} catch (PDOException $e) {
+    die("Erro DB: " . $e->getMessage());
+}
+
+// função helper para escapar saída
+function e($str) {
+    return htmlspecialchars($str, ENT_QUOTES, 'UTF-8');
+}
+
+// checar se usuário está autenticado
+function ensure_logged_in() {
+    if (empty($_SESSION['user_id'])) {
+        header('Location: login.php');
+        exit;
+    }
+}
+?>
