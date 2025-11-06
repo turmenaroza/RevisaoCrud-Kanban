c40d147e295748a4c5c174620e02da1d8cbe2ccd
Julia Izabel <julia_roza@estudante.sesisenai.org.br>
2025-11-06 09:13:57 -0300

    add insert into para alguns usuarios e tarefas

--- banco.sql
+++ banco.sql
@@ -19,3 +19,16 @@ CREATE TABLE tarefas (
     status ENUM('a fazer', 'fazendo', 'pronto') NOT NULL DEFAULT 'a fazer',
     FOREIGN KEY (id_usuario) REFERENCES usuarios(id) ON DELETE CASCADE
 );
+
+INSERT INTO usuarios (nome, email) VALUES
+('Julia Roza', 'julia_roza@gmail.com'),
+('Maria Luiza', 'maria.luiza@gmail.com'),
+('Victoria Fernanda', 'Victoria.fer@gmail.com');
+
+INSERT INTO tarefas (usuario_id, descricao, nome_setor, prioridade, status) VALUES
+(1, 'Revisar relatórios financeiros do último trimestre', 'Financeiro', 'alta', 'a fazer'),
+(1, 'Atualizar planilha de controle de despesas', 'Financeiro', 'média', 'fazendo'),
+(2, 'Organizar treinamento de integração para novos funcionários', 'RH', 'baixa', 'a fazer'),
+(2, 'Responder solicitações de férias pendentes', 'RH', 'média', 'pronto'),
+(3, 'Corrigir falha no sistema de login do portal interno', 'TI', 'alta', 'fazendo'),
+(3, 'Testar novo módulo de cadastro de clientes', 'Comercial', 'média', 'a fazer');
